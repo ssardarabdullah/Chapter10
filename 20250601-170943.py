5e62604988a5dec435169e866578a32cd6d3bbb9
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-06-01 17:20:27 +0500

    Update 20250601-170943.pychapter10

--- 20250601-170943.py
+++ 20250601-170943.py
@@ -1,21 +1,28 @@
+Question 5_______
+Combine the two programs from If 
+the number is already stored, 
+report the favorite number to the
+user. If not,
+prompt for the user’s favorite
+number and store it in a file. Run
+the program twice to 
+see that it works.
+
+solution __________
 import json
 import os
-
 filename = "favorite_number.json"
-
 def get_favorite_number():
     try:
         with open(filename) as f:
             return json.load(f)
     except FileNotFoundError:
         return None
-
 def ask_and_store_number():
     number = input("What is your favorite number? ")
     with open(filename, "w") as f:
         json.dump(number, f)
     return number
-
 def main():
     number = get_favorite_number()
     if number:
