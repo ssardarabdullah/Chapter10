f8e4a810eb793f02a72d7c22cd34310f27af629b
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-06-01 17:00:51 +0500

    Update 20250601-165714.pychapter10

--- 20250601-165714.py
+++ 20250601-165714.py
@@ -1,4 +1,11 @@
-# 10-3: Guest
+Question 1_______
+ Write a program that prompts 
+the user for their name.
+When theyrespond, write their name to a 
+file called guest.txt.
+
+solution _______
+Guest
 name = input("What is your name? ")
 with open("guest.txt", "w") as file:
     file.write(name)
