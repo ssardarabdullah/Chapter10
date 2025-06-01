2a89263aace3128a6fbd3c5291acef17d28692cb
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-06-01 17:03:32 +0500

    Update 20250601-165738.pychapter10

--- 20250601-165738.py
+++ 20250601-165738.py
@@ -1,4 +1,15 @@
-# 10-4: Guest Book
+Question 2 ______
+Write a while loop that prompts
+users for their name. When
+they enter their name, print a
+greeting to the screen and add a
+line recordingtheir visit in a file called
+guest_book.txt. Make sure each
+entry appears on anew line in the file.
+
+
+solution _______
+Guest Book
 print("Enter 'q' to quit.")
 while True:
     name = input("What is your name? ")
