75cf6893d8401bde621d17411b16de40f368c05f
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-06-01 17:14:30 +0500

    Update 20250601-170914.pychapter10

--- 20250601-170914.py
+++ 20250601-170914.py
@@ -1,10 +1,20 @@
-import json
+Question 4_________
 
+ Write a program that prompts
+     for the user’s favorite
+number. Use json.dump() to store 
+this number in a file. Write a
+separate pro￾gram that reads in 
+this value and prints the message,
+“I know your favorite number!
+
+
+Solution __________
+import json
 number = input("What is your favorite number? ")
 with open("favorite_number.json", "w") as f:
     json.dump(number, f)
     import json
-
 try:
     with open("favorite_number.json") as f:
         number = json.load(f)
