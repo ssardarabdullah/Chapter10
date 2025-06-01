61486ae29e8a96bf10a7a7db6c4859da3a4d2f2d
ssardarabdullah <ssardarabdulrehman58@gmail.com>
2025-06-01 17:06:01 +0500

    Update 20250601-165803.pychapter10

--- 20250601-165803.py
+++ 20250601-165803.py
@@ -1,4 +1,13 @@
-# 10-5: Programming Poll
+Question 3_____
+Write a while loop that asks
+people why they like
+programming. Each time someone
+enters a reason, add their
+reason to a filethat stores all the responses.
+
+
+solution ______
+ Programming Poll
 print("Enter 'q' to quit.")
 while True:
     reason = input("Why do you like programming? ")
